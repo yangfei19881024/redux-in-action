7b1a8a50d745da583200ff45d506e2cf641a937f
杨飞 <yangfei@boqii.net>
2016-04-29 00:10:04 +0800

    first commit

---
+++ src/components/post_index.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
+import { bindActionCreators } from 'redux';
+import { Link } from 'react-router';
+
+class PostsIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <Link to={`posts/${post.id}`}>
+          <span className="pull-xs-right">{post.categories}</span>
+          <strong>{post.title}</strong>
+          </Link>
+        </li>
+        );
+    });
+  }
+
+  render() {
+
+    if( this.props.posts.length == 0 ){
+      return <div>加载中....</div>;
+    }
+    return (
+      <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a Post
+          </Link>
+        </div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { posts: state.posts.all };
+}
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({fetchPosts},dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostsIndex);
